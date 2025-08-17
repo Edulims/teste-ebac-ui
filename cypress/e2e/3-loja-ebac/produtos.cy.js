2cdb5d941ee8e4c8818394360b5b1f1f4c4a54fa
Eduardo Lima <educmlima@gmail.com>
2025-08-17 16:47:43 -0300

    teste com page-objects e fixtures(produtos)

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,47 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/Produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Atlas Fitness Tank')
+        cy.get('.product_title').should('contain' , 'Atlas Fitness Tank')
+    });
+
+    it('Deve buscar um prouto com sucesso', () => {
+        let produto = 'Apollo Running'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve visitar a página do produto ', () => {
+        produtosPage.visitarProduto('Celeste Sports Bra')
+        cy.get('.product_title').should('contain' , 'Celeste Sports Bra')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        produtosPage.addProdutoCarrinho('S' , 'Brown' , qtd)
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[2].tamanho,
+            dados[2].cor,
+            dados[2].quantidade
+        )
+        cy.get('.woocommerce-message').should('contain' , dados[2].nomeProduto)
+        }) 
+
+
         
-        cy.get('.product-block')
-            //.first() -primeiro
-            //.last() -ultimo
-            //.eq(2) -posição
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
-
-        //cy.get('#tab-title-description').should('exist')
-        cy.get('#tab-title-description').should('contain','Descrição')
-
     });
 });
